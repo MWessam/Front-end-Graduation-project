371c562b41e30c1cac5003e4746faf6c3d28d8da
Wessam <mohwessam@std.mans.edu.eg>
2026-05-16 13:55:31 +0300

    fixing

--- Eureka/vite.config.js
+++ Eureka/vite.config.js
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Front-end-Graduation-project/',
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
